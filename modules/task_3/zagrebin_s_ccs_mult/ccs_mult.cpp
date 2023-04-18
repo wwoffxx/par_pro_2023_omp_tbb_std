cc7c2efb2ef463edd95494be5b657d0db9744b31
SergeyZagrebin <sergey.127.0.0.1@ya.ru>
2023-04-18 22:47:51 +0300

    Fix strange ifndef

--- modules/task_3/zagrebin_s_ccs_mult/ccs_mult.cpp
+++ modules/task_3/zagrebin_s_ccs_mult/ccs_mult.cpp
@@ -1,7 +1,7 @@
 // Copyright 2023 Zagrebin S.
 
-#include "../../../modules/task_3/zagrebin_s_ccs_mult/ccs_mult.h"
 #include "tbb/parallel_for.h"
+#include "../../../modules/task_3/zagrebin_s_ccs_mult/ccs_mult.h"
 #include <list>
 #include <random>
 
