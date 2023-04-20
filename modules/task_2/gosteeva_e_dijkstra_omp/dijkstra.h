8d913b516df71d6a78f813bfe6ec9cd6ac8b29bf
Kat-G <katylaky@outlook.com>
2023-04-20 17:11:12 +0300

    c2

--- modules/task_2/gosteeva_e_dijkstra_omp/dijkstra.h
+++ modules/task_2/gosteeva_e_dijkstra_omp/dijkstra.h
@@ -1,14 +1,13 @@
 // Copyright 2023 Gosteeva Ekaterina
-#ifndef MODULES_TASK_2_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H_
-#define MODULES_TASK_2_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H_
+#ifndef MODULES_TASK_2_GOSTEEVA_E_DIJKSTRA_OMP_DIJKSTRA_H_
+#define MODULES_TASK_2_GOSTEEVA_E_DIJKSTRA_OMP_DIJKSTRA_H_
 
 #include <omp.h>
 #include <climits>
 #include <vector>
 
-std::vector<int> DijkstraSeq(std::vector<int> graph, 
+std::vector<int> DijkstraSeq(std::vector<int> graph,
     int source, int size);
-std::vector<int> DijkstraParallel(std::vector<int> graph, 
+std::vector<int> DijkstraParallel(std::vector<int> graph,
     int source, int size);
-    
-#endif  // MODULES_TASK_2_GOSTEEVA_E_DIJKSTRA_DIJKSTRA_H_
+#endif  // MODULES_TASK_2_GOSTEEVA_E_DIJKSTRA_OMP_DIJKSTRA_H_
