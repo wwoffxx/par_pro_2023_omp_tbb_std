a01a88ee1d52508e2a786590190e2733f515405f
TibidoX <vovaboch0@gmail.com>
2023-04-21 23:59:58 +0300

    v1.01

--- modules/task_3/bochkarev_v_integral_tr/integral.cpp
+++ modules/task_3/bochkarev_v_integral_tr/integral.cpp
@@ -6,27 +6,28 @@
 #include "../../../modules/task_3/bochkarev_v_integral_tr/integral.h"
 
 class trFunctor {
-private:
-    double hx,hy,hz;
+ private:
+    double hx, hy, hz;
     double a, c, e;
     double res;
     const std::function<double(double, double, double)> &fun;
 
-public:
-    explicit trFunctor(double _hx, double _hy, double _hz, double _a, double _c, double _e, 
+ public:
+    explicit trFunctor(double _hx, double _hy, double _hz, double _a, double _c, double _e,
     const std::function<double(double, double, double)> &_fun):
     hx(_hx), hy(_hy), hz(_hz), a(_a), c(_c), e(_e), res(0), fun(_fun) {}
 
-    trFunctor(const trFunctor& f, tbb::split): hx(f.hx), hy(f.hy), hz(f.hz), a(f.a), c(f.c), e(f.e), res(0), fun(f.fun) {}
+    trFunctor(const trFunctor& f, tbb::split): hx(f.hx), hy(f.hy), hz(f.hz),
+    a(f.a), c(f.c), e(f.e), res(0), fun(f.fun) {}
 
     void operator()(const tbb::blocked_range3d<int>& r) {
         int i = r.pages().begin(), i_end = r.pages().end();
         int j = r.rows().begin(), j_end = r.rows().end();
         int k = r.cols().begin(), k_end = r.cols().end();
 
-        for(i = r.pages().begin(); i < i_end; i++)
-            for(j = r.rows().begin(); j < j_end; j++)
-                for(k = r.cols().begin(); k < k_end; k++) {
+        for (i = r.pages().begin(); i < i_end; i++)
+            for (j = r.rows().begin(); j < j_end; j++)
+                for (k = r.cols().begin(); k < k_end; k++) {
                     const double x1 = a + i*hx;
                     const double x2 = a + (i+1)*hx;
 
