79dbcca8a92b1063507820ed90df422fa258e93c
Polina-Petrova07 <polina_petrova_02@bk.ru>
2023-04-23 20:54:42 +0300

    del time

--- modules/task_3/busko_p_operator_sobelya/main.cpp
+++ modules/task_3/busko_p_operator_sobelya/main.cpp
@@ -1,6 +1,6 @@
 // Copyright 2023 Busko Polina
 
-#include <omp.h>
+// #include <omp.h>
 #include <gtest/gtest.h>
 #include <iostream>
 #include "./operator_sobelya.h"
@@ -8,100 +8,100 @@
 TEST(SobelFilterTBB, matrix_100_on_100) {
     int size = 100;
     Img image(size, size, 1);
-    double start;
-    double end;
+    // double start;
+    // double end;
 
-    start = omp_get_wtime();
+    // start = omp_get_wtime();
     std::vector<std::vector<int>> seq_answer = image.SeqSobel();
-    end = omp_get_wtime();
-    std::cout << "Time of the sequential algorithm = " << end - start;
-    std::cout << std::endl;
+    // end = omp_get_wtime();
+    // std::cout << "Time of the sequential algorithm = " << end - start;
+    // std::cout << std::endl;
 
-    start = omp_get_wtime();
+    // start = omp_get_wtime();
     std::vector<std::vector<int>> parallel_answer = image.ParallelSobel();
-    end = omp_get_wtime();
-    std::cout << "Time of the parallel algorithm = " << end - start;
-    std::cout << std::endl;
+    // end = omp_get_wtime();
+    // std::cout << "Time of the parallel algorithm = " << end - start;
+    // std::cout << std::endl;
 
     ASSERT_EQ(seq_answer, parallel_answer);
 }
 TEST(SobelFilterTBB, matrix_1000_on_1000) {
     int size = 1000;
     Img image(size, size, 1);
-    double start;
-    double end;
+    // double start;
+    // double end;
 
-    start = omp_get_wtime();
+    // start = omp_get_wtime();
     std::vector<std::vector<int>> seq_answer = image.SeqSobel();
-    end = omp_get_wtime();
-    std::cout << "Time of the sequential algorithm = " << end - start;
-    std::cout << std::endl;
+    // end = omp_get_wtime();
+    // std::cout << "Time of the sequential algorithm = " << end - start;
+    // std::cout << std::endl;
 
-    start = omp_get_wtime();
+    // start = omp_get_wtime();
     std::vector<std::vector<int>> parallel_answer = image.ParallelSobel();
-    end = omp_get_wtime();
-    std::cout << "Time of the parallel algorithm = " << end - start;
-    std::cout << std::endl;
+    // end = omp_get_wtime();
+    // std::cout << "Time of the parallel algorithm = " << end - start;
+    // std::cout << std::endl;
 
     ASSERT_EQ(seq_answer, parallel_answer);
 }
 TEST(SobelFilterTBB, random_matrix_100_on_100) {
     int size = 100;
     Img image(size, size);
-    double start;
-    double end;
+    // double start;
+    // double end;
 
-    start = omp_get_wtime();
+    // start = omp_get_wtime();
     std::vector<std::vector<int>> seq_answer = image.SeqSobel();
-    end = omp_get_wtime();
-    std::cout << "Time of the sequential algorithm = " << end - start;
-    std::cout << std::endl;
+    // end = omp_get_wtime();
+    // std::cout << "Time of the sequential algorithm = " << end - start;
+    // std::cout << std::endl;
 
-    start = omp_get_wtime();
+    // start = omp_get_wtime();
     std::vector<std::vector<int>> parallel_answer = image.ParallelSobel();
-    end = omp_get_wtime();
-    std::cout << "Time of the parallel algorithm = " << end - start;
-    std::cout << std::endl;
+    // end = omp_get_wtime();
+    // std::cout << "Time of the parallel algorithm = " << end - start;
+    // std::cout << std::endl;
 
     ASSERT_EQ(seq_answer, parallel_answer);
 }
 TEST(SobelFilterTBB, random_matrix_500_on_500) {
     int size = 500;
     Img image(size, size);
-    double start;
-    double end;
+    // double start;
+    // double end;
 
-    start = omp_get_wtime();
+    // start = omp_get_wtime();
     std::vector<std::vector<int>> seq_answer = image.SeqSobel();
-    end = omp_get_wtime();
-    std::cout << "Time of the sequential algorithm = " << end - start;
-    std::cout << std::endl;
+    // end = omp_get_wtime();
+    // std::cout << "Time of the sequential algorithm = " << end - start;
+    // std::cout << std::endl;
 
-    start = omp_get_wtime();
+    // start = omp_get_wtime();
     std::vector<std::vector<int>> parallel_answer = image.ParallelSobel();
-    end = omp_get_wtime();
-    std::cout << "Time of the parallel algorithm = " << end - start;
-    std::cout << std::endl;
+    // end = omp_get_wtime();
+    // std::cout << "Time of the parallel algorithm = " << end - start;
+    // std::cout << std::endl;
 
     ASSERT_EQ(seq_answer, parallel_answer);
 }
 TEST(SobelFilterTBB, random_matrix_1000_on_1000) {
     int size = 1000;
     Img image(size, size);
-    double start;
-    double end;
+    // double start;
+    // double end;
 
-    start = omp_get_wtime();
+    // start = omp_get_wtime();
     std::vector<std::vector<int>> seq_answer = image.SeqSobel();
-    end = omp_get_wtime();
-    std::cout << "Time of the sequential algorithm = " << end - start;
-    std::cout << std::endl;
+    // end = omp_get_wtime();
+    // std::cout << "Time of the sequential algorithm = " << end - start;
+    // std::cout << std::endl;
 
-    start = omp_get_wtime();
+    // start = omp_get_wtime();
     std::vector<std::vector<int>> parallel_answer = image.ParallelSobel();
-    end = omp_get_wtime();
-    std::cout << "Time of the parallel algorithm = " << end - start;
-    std::cout << std::endl;
+    // end = omp_get_wtime();
+    // std::cout << "Time of the parallel algorithm = " << end - start;
+    // std::cout << std::endl;
 
     ASSERT_EQ(seq_answer, parallel_answer);
 }
