656914847b293ac90a28bdf59fc096dc93e9f90d
SigachevAV <43341367+SigachevAV@users.noreply.github.com>
2023-04-24 23:48:29 +0300

    style fix

--- modules/task_2/sigachev_a_shell_sort_odd_even_merge_omp/sort.h
+++ modules/task_2/sigachev_a_shell_sort_odd_even_merge_omp/sort.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Sigachev Anton
-#ifndef MODULES_TASK_2_SIGACHEV_A_SHELL_SORT_ODD_EVEN_MERGE_SORT_H_
-#define MODULES_TASK_2_SIGACHEV_A_SHELL_SORT_ODD_EVEN_MERGE_SORT_H_
+#ifndef MODULES_TASK_2_SIGACHEV_A_SHELL_SORT_ODD_EVEN_MERGE_OMP_SORT_H_
+#define MODULES_TASK_2_SIGACHEV_A_SHELL_SORT_ODD_EVEN_MERGE_OMP_SORT_H_
 
 #include <omp.h>
 #include <iostream>
@@ -11,4 +11,4 @@ void ShellSortSequantial(std::vector<int>* vec);
 void ShellSortParallel(std::vector<int>* vec);
 bool checkSort(const std::vector<int>& vec);
 
-#endif  // MODULES_TASK_2_SIGACHEV_A_SHELL_SORT_ODD_EVEN_MERGE_SORT_H_
+#endif  // MODULES_TASK_2_SIGACHEV_A_SHELL_SORT_ODD_EVEN_MERGE_OMP_SORT_H_
