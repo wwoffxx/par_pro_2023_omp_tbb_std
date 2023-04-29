a93768be93f3d3461502fa0ea5f0ac33cf3077da
Пашина Алина <nice.alina28@mail.ru>
2023-04-29 17:03:29 +0300

    commit omp code

---
+++ modules/task_2/pashina_a_sparse_matrix_omp/CRSMatrix_omp.h
@@ -0,0 +1,31 @@
+// Copyright 2023 Pashina Alina
+#ifndef PAR_PRO_2023_OMP_TBB_STD_MODULES_TASK_2_PASHINA_A_SPARSE_MATRIX_OMP_CRSMATRIX_OMP_H_
+#define PAR_PRO_2023_OMP_TBB_STD_MODULES_TASK_2_PASHINA_A_SPARSE_MATRIX_OMP_CRSMATRIX_OMP_H_
+
+#include <string>
+#include <vector>
+
+class CRSMatrix {
+ public:
+  int numRow, numCol;
+  std::vector<double> valueCRS;
+  std::vector<int> colsCRS;
+  std::vector<int> pointerCRS;
+
+  CRSMatrix(int numC, int numR, const std::vector<double>& myVal,
+            const std::vector<int>& myColu, const std::vector<int>& myPointer);
+  explicit CRSMatrix(int numC = 0, int numR = 0);
+  explicit CRSMatrix(std::vector<std::vector<double>> matr);
+  bool operator==(const CRSMatrix& matr) const;
+  CRSMatrix MatrixTransp();
+  CRSMatrix MatrixMultOMP(CRSMatrix matr);
+};
+
+std::vector<std::vector<double>> fillZero(int cols, int rows);
+std::vector<std::vector<double>> createRandomMatrix(int cols, int rows,
+                                                    double perc);
+std::vector<std::vector<double>> multiplyVecMatrix(
+    std::vector<std::vector<double>> myFirstMatrix,
+    std::vector<std::vector<double>> mySecondMatrix);
+
+#endif  // PAR_PRO_2023_OMP_TBB_STD_MODULES_TASK_2_PASHINA_A_SPARSE_MATRIX_OMP_CRSMATRIX_OMP_H_
