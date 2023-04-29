ef7f12d84299fbed7b1650eefb56ec2ae99abab1
aleksandarsmnvc <36212895+aleksandarsmnvc@users.noreply.github.com>
2023-04-29 19:07:34 +0300

    Add files via upload

---
+++ modules/task_2/simeunovic_a_gauss_horizontal_filter/gauss_horizontal_filter.h
@@ -0,0 +1,18 @@
+// Copyright 2023 Simeunovic Aleksandar
+#ifndef MODULES_TASK_2_SIMEUNOVIC_A_GAUSS_HORIZONTAL_FILTER_GAUSS_HORIZONTAL_FILTER_H_
+#define MODULES_TASK_2_SIMEUNOVIC_A_GAUSS_HORIZONTAL_FILTER_GAUSS_HORIZONTAL_FILTER_H_
+#include <cstdint>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <random>
+#include <cinttypes>
+
+int GetIndex(int i, int j, int offset);
+std::vector<uint8_t> GetRandMatrix(int offset, int pixelHeight);
+std::vector<uint8_t> Filter(std::vector<uint8_t> srcVec, int offset,
+    int pixelHeight, double sigma = 1.0);
+std::vector<uint8_t> ParFilter(std::vector<uint8_t> srcVec, int offset,
+    int pixelHeight, int threads = 2, double sigma = 1.0);
+
+#endif  // _MODULES_TASK_2_SIMEUNOVIC_A_GAUSS_HORIZONTAL_FILTER_GAUSS_HORIZONTAL_FILTER_H_
