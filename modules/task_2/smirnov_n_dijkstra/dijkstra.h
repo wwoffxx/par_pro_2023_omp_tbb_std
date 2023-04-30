97abc552ce94628b81b7a91c28df9c796c6e779a
Nick_022 <nick.smirnov022@gmail.com>
2023-04-30 12:23:52 +0300

    2

---
+++ modules/task_2/smirnov_n_dijkstra/dijkstra.h
@@ -0,0 +1,16 @@
+// Copyright 2023 Smirnov Nick
+#ifndef MODULES_TASK_2_SMIRNOV_N_DIJKSTRA_OMP_DIJKSTRA_OMP_H_
+#define MODULES_TASK_2_SMIRNOV_N_DIJKSTRA_OMP_DIJKSTRA_OMP_H_
+
+#include <string>
+#include <vector>
+
+std::vector<std::vector<int>> getRandomVector(const int count);
+std::vector<int> getDijkstra(const std::vector<std::vector<int>>& graf, const int top);
+std::vector<int> getSequentialDijkstra(const std::vector<std::vector<int>>& graf);
+std::vector<int> getParallelDijkstra(const std::vector<std::vector<int>>& graf);
+
+#endif  // MODULES_TASK_2_SMIRNOV_N_DIJKSTRA_OMP_DIJKSTRA_OMP_H_
+
+
+
