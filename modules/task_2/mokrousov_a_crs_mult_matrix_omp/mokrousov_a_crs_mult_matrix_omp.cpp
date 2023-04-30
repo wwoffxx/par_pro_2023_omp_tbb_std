ee0e03778584f5228a000685f7da055378332f23
wayr8 <92333451+wayr8@users.noreply.github.com>
2023-04-30 13:24:36 +0300

    mokrousov_a_crs_mult_matrix_omp
    mokrousov_a_crs_mult_matrix_omp

---
+++ modules/task_2/mokrousov_a_crs_mult_matrix_omp/mokrousov_a_crs_mult_matrix_omp.cpp
@@ -0,0 +1,38 @@
+// Copyright 2023 Mokrousov Artem
+
+#include "../../../modules/task_2/mokrousov_a_crs_mult_matrix_omp/mokrousov_a_crs_mult_matrix_omp.h"
+
+#include <omp.h>
+
+Matrix omp_crs_mult(Matrix A, Matrix B) {
+    B.transponse();
+    Matrix result;
+    int resIndex = 0;
+#pragma omp parallel for ordered
+    for (int i = 0; i < A.pointer.size() - 1; i++) {
+        for (int j = 0; j < B.pointer.size() - 1; j++) {
+            int sumVal = 0;
+            for (int AvalInd = A.pointer.at(i); AvalInd < A.pointer.at(i + 1);
+                 AvalInd++) {
+                for (int BvalInd = B.pointer.at(j);
+                     BvalInd < B.pointer.at(j + 1); BvalInd++) {
+                    if (A.colums.at(AvalInd) == B.colums.at(BvalInd)) {
+                        sumVal += A.values.at(AvalInd) * B.values.at(BvalInd);
+                    }
+                }
+            }
+#pragma omp ordered
+            {
+                if (result.pointer.size() == i)
+                    result.pointer.push_back(resIndex);
+                if (sumVal > 0) {
+                    result.values.push_back(sumVal);
+                    result.colums.push_back(j);
+                    resIndex++;
+                }
+            }
+        }
+    }
+    result.pointer.push_back(result.values.size());
+    return result;
+}
