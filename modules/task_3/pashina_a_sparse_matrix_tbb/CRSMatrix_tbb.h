35a37215e3e6c02099710bf97ff6222ea6894f20
Пашина Алина <nice.alina28@mail.ru>
2023-05-09 12:45:35 +0300

    first commit tbb

---
+++ modules/task_3/pashina_a_sparse_matrix_tbb/CRSMatrix_tbb.h
@@ -0,0 +1,31 @@
+// Copyright 2023 Pashina Alina
+#ifndef MODULES_TASK_3_PASHINA_A_SPARSE_MATRIX_TBB_CRSMATRIX_TBB_H_
+#define MODULES_TASK_3_PASHINA_A_SPARSE_MATRIX_TBB_CRSMATRIX_TBB_H_
+
+#include <string>
+#include <vector>
+
+class CRSMatrix {
+ public:
+  int numRow, numCol;
+  std::vector<double> valueCRS;
+  std::vector<int> colsCRS;
+  std::vector<int> pointerCRS;
+
+  CRSMatrix(int numC, int numR, const std::vector<double>& myVal,
+            const std::vector<int>& myColu, const std::vector<int>& myPointer);
+  explicit CRSMatrix(int numC = 0, int numR = 0);
+  explicit CRSMatrix(std::vector<std::vector<double>> matr);
+  bool operator==(const CRSMatrix& matr) const;
+  CRSMatrix MatrixTransp();
+  CRSMatrix MatrixMultTBB(CRSMatrix matr);
+};
+
+std::vector<std::vector<double>> fillZero(int cols, int rows);
+std::vector<std::vector<double>> createRandomMatrix(int cols, int rows,
+                                                    double perc);
+std::vector<std::vector<double>> multiplyVecMatrix(
+    std::vector<std::vector<double>> myFirstMatrix,
+    std::vector<std::vector<double>> mySecondMatrix);
+
+#endif  // MODULES_TASK_3_PASHINA_A_SPARSE_MATRIX_TBB_CRSMATRIX_TBB_H_
