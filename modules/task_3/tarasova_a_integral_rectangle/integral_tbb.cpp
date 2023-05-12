ff5016d4c0ea9460350fd36306b6323345b2318a
taranast <taranast434@gmail.com>
2023-05-12 11:45:57 +0300

    c1

--- modules/task_3/tarasova_a_integral_rectangle/integral_tbb.cpp
+++ modules/task_3/tarasova_a_integral_rectangle/integral_tbb.cpp
@@ -4,11 +4,11 @@
 #include "../../../modules/task_3/tarasova_a_integral_rectangle/integral_tbb.h"
 
 class CalculationXY {
-private:
+private: 
     double my_h;
     double my_a;
     std::vector<double>* my_x1;
-public:
+public: 
     void operator()(const tbb::blocked_range<size_t>& r) const {
         for (size_t i = r.begin(); i < r.end(); i++)
             (*my_x1)[i] = my_a + i * my_h + my_h / 2;
@@ -21,21 +21,21 @@ public:
 };
 
 class Sum {
-private:
+private: 
     double my_x;
     double my_y;
     double my_a;
     double* my_sum;
     double my_h;
     double (*my_f)(double, double, double);
-public:
+public: 
     void operator()(const tbb::blocked_range<size_t>& r) const {
         for (size_t i = r.begin(); i < r.end(); i++) {
             double z = my_a + i * my_h + my_h / 2;
             *my_sum += my_f(my_x, my_y, z) * my_h * my_h * my_h;
         }
     }
-    Sum(double& x, double& y, const double& a, const double& h, double* sum, double f(double, double, double)) {
+    Sum(const double& x, const double& y, const double& a, const double& h, double* sum, double f(double, double, double)) {
         my_x = x;
         my_y = y;
         my_a = a;
@@ -50,9 +50,9 @@ double getParallel(const double& a1, const double& a2, const double& a3, const d
     const double& b2, const double& b3, const double& h, double f(double, double, double)) {
     double sum = 0.0, x, y, z;
     int n1, n2, n3;
-    n1 = (int)((b1 - a1) / h);
-    n2 = (int)((b2 - a2) / h);
-    n3 = (int)((b3 - a3) / h);
+    n1 = static_cast<int>((b1 - a1) / h);
+    n2 = static_cast<int>((b2 - a2) / h);
+    n3 = static_cast<int>((b3 - a3) / h);
     std::vector<double> x1(n1), y1(n2), z1(n3);
     tbb::parallel_for(tbb::blocked_range<size_t>(0, n1), CalculationXY(h, a1, &x1));
     tbb::parallel_for(tbb::blocked_range<size_t>(0, n2), CalculationXY(h, a2, &y1));
@@ -66,9 +66,9 @@ double getSequential(const double& a1, const double& a2, const double& a3, const
     const double& b2, const double& b3, const double& h, double f(double, double, double)) {
     double sum = 0.0, x, y, z;
     int n1, n2, n3;
-    n1 = (int)((b1 - a1) / h);
-    n2 = (int)((b2 - a2) / h);
-    n3 = (int)((b3 - a3) / h);
+    n1 = static_cast<int>((b1 - a1) / h);
+    n2 = static_cast<int>((b2 - a2) / h);
+    n3 = static_cast<int>((b3 - a3) / h);
     std::vector<double> x1(n1), y1(n2), z1(n3);
     for (int i = 0; i < n1; i++) {
         x = a1 + i * h + h / 2;
