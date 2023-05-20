24a742d5eb7e6f4b20c44ddba889c341291463ba
FreeJatmen <freejatmen@gmail.com>
2023-05-20 04:00:18 +0300

    second

--- modules/task_3/nikitin_a_strassen_tbb/main.cpp
+++ modules/task_3/nikitin_a_strassen_tbb/main.cpp
@@ -63,6 +63,33 @@ TEST(Nikitin_test, NonPowerOf2Size) {
   }
 }
 
+TEST(Nikitin_test, ProvidedData) {
+  double a[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+  double b[16] = {2, 0, 1, 2, 1, 2, 1, 0, 0, 1, 2, 1, 2, 1, 0, 2};
+  double expected[16] = {10, 5,  6,  10, 26, 13, 18, 26,
+                         42, 21, 30, 42, 58, 29, 42, 58};
+  double c[16];
+
+  strassen(a, b, c, 4);
+
+  for (int i = 0; i < 16; i++) {
+    EXPECT_EQ(c[i], expected[i]);
+  }
+}
+
+TEST(Nikitin_test, ZeroMatrix) {
+  double a[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  double b[16] = {2, 0, 1, 2, 1, 2, 1, 0, 0, 1, 2, 1, 2, 1, 0, 2};
+  double expected[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  double c[16];
+
+  strassen(a, b, c, 4);
+
+  for (int i = 0; i < 16; i++) {
+    EXPECT_EQ(c[i], expected[i]);
+  }
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
