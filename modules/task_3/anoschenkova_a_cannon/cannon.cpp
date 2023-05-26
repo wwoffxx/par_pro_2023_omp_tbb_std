312e0da5cfdaa36ba4a1ebb801e3168299cfd2b8
Valentin <90253430+thenikto@users.noreply.github.com>
2023-05-26 09:55:37 +0300

    test 3

--- modules/task_3/anoschenkova_a_cannon/cannon.cpp
+++ modules/task_3/anoschenkova_a_cannon/cannon.cpp
@@ -72,9 +72,9 @@ matrix BlockMult(const matrix &A, const matrix &B, const int &blockSize) {
     matrix res(n, std::vector<double>(n, 0));
 
     for (int jj = 0; jj < n; jj += blockSize) {
-        jjMin = std::min(jj + blockSize, n);
+        jjMin = Min(jj + blockSize, n);
         for (int kk = 0; kk < n; kk += blockSize) {
-            kkMin = std::min(kk + blockSize, n);
+            kkMin = Min(kk + blockSize, n);
 
             tbb::parallel_for(tbb::blocked_range<int>(0, n, 2),
                               [&](tbb::blocked_range<int> range) {
