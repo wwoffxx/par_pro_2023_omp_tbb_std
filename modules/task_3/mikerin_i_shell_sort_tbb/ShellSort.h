0df7ff95ad480d79235f8623243a14863712d3f8
Mikerin Ilya <mikerin-2002@mail.ru>
2023-05-27 19:33:14 +0300

    second try

--- modules/task_3/mikerin_i_shell_sort_tbb/ShellSort.h
+++ modules/task_3/mikerin_i_shell_sort_tbb/ShellSort.h
@@ -2,14 +2,18 @@
 #ifndef MODULES_TASK_3_MIKERIN_I_SHELL_SORT_TBB_SHELLSORT_H_
 #define MODULES_TASK_3_MIKERIN_I_SHELL_SORT_TBB_SHELLSORT_H_
 
+#include <tbb/tbb.h>
+#include<tbb/parallel_invoke.h>
 #include <vector>
 #include <string>
 #include <random>
 #include <iostream>
-#include <tbb/tbb.h>
-#include<tbb/parallel_invoke.h>
+
 #include "tbb/parallel_for.h"
 #include "tbb/blocked_range.h"
+
+
+
 
 std::vector<double> getRandomVector(int  sz);
 
