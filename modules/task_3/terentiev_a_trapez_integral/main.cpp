c28d7d89f002e8a821e1ef3cbcc5c4a871fd201e
Valentin <70850885+Alexanr13213@users.noreply.github.com>
2023-05-28 18:24:39 +0300

    t3_2

--- modules/task_3/terentiev_a_trapez_integral/main.cpp
+++ modules/task_3/terentiev_a_trapez_integral/main.cpp
@@ -11,7 +11,6 @@ TEST(Trapez_Integral, Test2_seq) {
     ASSERT_TRUE(Testing(1, 1, 1, 2, 2, 2, Func1, 3.375));
 }
 
-
 TEST(Trapez_Integral, Test3_seq) {
     ASSERT_TRUE(Testing(5, 0, 1, 7, 3, 2, Func1, 81));
 }
