63e27e2cb5e13cd47bcff2d1b7ff14bf7bad7895
rezchdmi <rezchdmi@fit.cvut.cz>
2023-05-28 20:39:07 +0300

    initial commit lr3

---
+++ modules/task_3/rezchikov_d_radix_simple_tbb/ops.h
@@ -0,0 +1,16 @@
+// Copyright 2023 Rezchikov Dmitrii
+#pragma once
+
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <random>
+#include <iostream>
+
+void countingSortSeq(std::vector<int> * array, int l, int r, int place);
+void countingSortPar(std::vector<int> * array, int l, int r, int place);
+void radSortSeq(std::vector<int> * vec);
+std::vector<int> radSortParMerge(std::vector<int> * vec);
+std::vector<int> getRndVector(int size);
+std::vector<int> merge(std::vector<int> * v1, std::vector<int> * v2);
