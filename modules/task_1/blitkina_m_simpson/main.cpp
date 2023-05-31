2fbd39c33fbe411c688895a3d17125eac47fd687
mariablitkina <mariablit@mail.ru>
2023-05-31 22:01:36 +0300

    Task 1 to rerun actions

--- modules/task_1/blitkina_m_simpson/main.cpp
+++ modules/task_1/blitkina_m_simpson/main.cpp
@@ -47,22 +47,6 @@ TEST(Simson_Method_Seq, Can_Integrate_Two_Dim_Func_3) {
     EXPECT_NEAR(res, 0.125, 1);
 }
 
-// TEST(Simson_Method_Seq, Can_Integrate_Three_Dim_Func) {
-//     std::function<double(const std::vector<double>&)> f = [](const std::vector<double>& vec){
-//         return sin(vec[0]) * vec[1] * cos(vec[2]);};  // f(x,y,z) = sin(x) * y * cos(z)
-//     std::vector<std::pair<double, double>> scope = { {0, 1}, {-2, 2}, {3, 7} };  // expected 0
-//     double res = calcIntegral(scope, f);
-//     EXPECT_NEAR(res, 0.0, 1);
-// }
-
-// TEST(Simson_Method_Seq, Can_Integrate_Three_Dim_Func_2) {
-//     std::function<double(const std::vector<double>&)> f = [](const std::vector<double>& vec) {
-//         return vec[0] + vec[1] + vec[2];};  // f(x,y,z) = x + y + z
-//     std::vector<std::pair<double, double>> scope = { {0, 1}, {0, 0.5}, {0, 0.5} };  // expected 0.25
-//     double res = calcIntegral(scope, f);
-//     EXPECT_NEAR(res, 0.25, 1);
-// }
-
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
