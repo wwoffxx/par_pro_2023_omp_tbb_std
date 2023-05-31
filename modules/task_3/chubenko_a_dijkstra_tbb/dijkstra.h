d050af46205580688b1b47af0dc90bbdae7e97d6
Andrey Chubenko <81777187+snakeboby@users.noreply.github.com>
2023-05-31 16:13:34 +0300

    Update dijkstra.h

--- modules/task_3/chubenko_a_dijkstra_tbb/dijkstra.h
+++ modules/task_3/chubenko_a_dijkstra_tbb/dijkstra.h
@@ -1,9 +1,8 @@
 // Copyright 2023 Chubenko Andrey
 
-#ifndef MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_OMP_DIJKSTRA_H_
-#define MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_OMP_DIJKSTRA_H_
+#ifndef MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_TBB_DIJKSTRA_H_
+#define MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_TBB_DIJKSTRA_H_
 
-#include <omp.h>
 #include <tbb/tbb.h>
 #include <climits>
 #include <vector>
@@ -15,4 +14,4 @@ std::vector<int> get_dists_dijkstra(std::vector<int> graph,
 std::vector<int> get_dists_dijkstra_parallel(std::vector<int> graph,
     int size, int start);
 
-#endif  // MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_OMP_DIJKSTRA_H_
+#endif  // MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_TBB_DIJKSTRA_H_
