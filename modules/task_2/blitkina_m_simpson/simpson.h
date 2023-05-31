4bb42132b168c5b7f21532f34ee436fe44a8bbfb
mariablitkina <mariablit@mail.ru>
2023-05-31 20:36:03 +0300

    Task 2 fix

--- modules/task_2/blitkina_m_simpson/simpson.h
+++ modules/task_2/blitkina_m_simpson/simpson.h
@@ -5,6 +5,8 @@
 #include <functional>
 #include <utility>
 
+using std::size_t;
+
 double calcIntegral(const std::vector<std::pair<double, double>>& scope,
         std::function<double(const std::vector<double>)> f,
         int accurancy = 100, size_t level = 0,
