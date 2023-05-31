5f56a72ae13016355493485c6dddca03c594727e
snakeboby <andreychubenko11@gmail.com>
2023-05-31 16:00:21 +0300

    add new file tbb

---
+++ modules/task_3/chubenko_a_dijkstra_tbb/dijkstra.h
@@ -0,0 +1,18 @@
+// Copyright 2023 Chubenko Andrey
+
+#ifndef MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_OMP_DIJKSTRA_H_
+#define MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_OMP_DIJKSTRA_H_
+
+#include <omp.h>
+#include <tbb/tbb.h>
+#include <climits>
+#include <vector>
+#include <iostream>
+
+std::vector<int> get_dists_dijkstra(std::vector<int> graph,
+    int size, int start);
+
+std::vector<int> get_dists_dijkstra_parallel(std::vector<int> graph,
+    int size, int start);
+
+#endif  // MODULES_TASK_3_CHUBENKO_A_DIJKSTRA_OMP_DIJKSTRA_H_
